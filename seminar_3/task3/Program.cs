b8c91781f37fb13f31b322333c0cf7ecf1a6d7ed
Denis Shamrov <d_shamrov@mail.ru>
2023-01-25 18:48:22 +0300

    Added completed seminar3 task3

---
+++ seminar_3/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        System.Console.WriteLine($"{Math.Pow(i , 3)}");
+    }
+}
+else
+{
+    System.Console.WriteLine($"{num} - не корректное число");
+}
+
