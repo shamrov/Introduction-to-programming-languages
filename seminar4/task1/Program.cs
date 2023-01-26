61ed680805d5b8d26e0ec0517cc02de870c8ea59
Denis Shamrov <d_shamrov@mail.ru>
2023-01-26 22:31:24 +0300

    Added completed seminar4 task1

---
+++ seminar4/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Exponentiation(int num, int degree)
+{
+    int result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result *= num;
+    }
+return result;
+}
+
+System.Console.WriteLine(Exponentiation(3,5));
+
