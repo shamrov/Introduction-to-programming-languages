c1e7738e1bab6ac88d76a9e01fd3929b03f65614
Denis Shamrov <d_shamrov@mail.ru>
2023-02-13 21:50:25 +0300

    Added Completed seminar9 task2

---
+++ seminar9/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FuncAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return FuncAkkerman(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return FuncAkkerman(m - 1, FuncAkkerman(m, n - 1));
+    }
+    return 0;
+}
+
+int result = FuncAkkerman(3, 2);
+System.Console.WriteLine(result);
