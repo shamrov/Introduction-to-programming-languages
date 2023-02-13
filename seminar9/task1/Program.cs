f9742d1d0cf7347ac0b497ee929510782cc6662f
Denis Shamrov <d_shamrov@mail.ru>
2023-02-13 21:50:01 +0300

    Added Completed seminar9 task1

---
+++ seminar9/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int SumElement(int m, int n)
+{
+    if (m < n)
+    {
+        return n + SumElement(m, n - 1); 
+    }
+    return n;
+}
+
+int num = SumElement(1,15);
+System.Console.WriteLine(num);
